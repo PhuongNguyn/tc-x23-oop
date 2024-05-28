7803060d0cc3d6b1ecc5ba8dcf9c5b6ebc546407
Phuong Nguyen <zinzin.vung@gmail.com>
2024-05-28 20:12:31 +0700

    update encapsulation and extension

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 class Product {
-  name: string;
-  price: number;
-  quantity: number;
+  // access modifier
+  // private -> khong truy cap tu ben ngoai vao duoc
+  // protected -> chi co lop con ke thua moi co the truy cap duoc
+  // public -> co the truy cap duoc o bat cu dau
+  private name: string;
+  private price: number;
+  private quantity: number;
 
   constructor(name: string, price: number, quantity: number) {
     this.name = name;
@@ -9,26 +13,31 @@ class Product {
     this.quantity = quantity;
   }
 
-  getPrice() {
-    return this.price;
-  }
 
   getName() {
     return this.name;
+  }
+
+  getPrice() {
+    return this.price;
   }
 
   caculatePrice() {
     return this.price;
   }
 }
-
 class User {
-  name: string;
-  age: number;
+  private name: string;
+  private age: number;
 
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
+  }
+
+
+  setName(name: string) {
+    this.name = name.toLowerCase()
   }
 
   getUserName() {
@@ -40,10 +49,70 @@ class User {
   }
 }
 
-const user1 = new User("A", 10);
-
-const product1 = new Product("Car", 1000, 10);
+// create instance of class
 const product2 = new Product("Car 2", 2000, 20);
 
-console.log(`Username: ${user1.getUserName()}`);
-console.log(`ProductName: ${product1.getName()}`);
+class Animals {
+  private age: number;
+  private eyes: number;
+
+  constructor(age: number, eyes: number) {
+    this.age = age;
+    this.eyes = eyes;
+  }
+
+  setAge(age: number) {
+    this.age = age
+  }
+
+  getAge() {
+    return this.age
+  }
+
+}
+
+
+
+class Dog extends Animals {
+  private legs: number;
+
+  constructor(age: number, legs: number, eyes: number) {
+    super(age, eyes)
+    this.legs = legs
+  }
+
+
+  run() { }
+
+}
+
+class Cat extends Animals {
+  private legs: number;
+
+  constructor(age: number, legs: number, eyes: number) {
+    super(age, eyes)
+    this.legs = legs
+  }
+
+
+  jump() {
+
+  }
+
+}
+
+class Bird extends Animals {
+  private wings: number;
+
+  constructor(age: number, wings: number, eyes: number) {
+    super(age, eyes)
+    this.wings = wings
+  }
+
+  fly() {
+
+  }
+}
+
+const bird = new Bird(5, 2, 2)
+bird.getAge()
